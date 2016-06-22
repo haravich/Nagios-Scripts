282a61a0ad031c44a92e5a7d0bd8ae3bd9f684f3
udthariprasath <udthariprasath@gmail.com>
2016-06-22 18:14:39 +0530

    Added License
    It works on Bash environment

--- check_ram.sh
+++ check_ram.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 #!/bin/bash
+#*-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**
+# 
+#       Script:         check_ram.sh
+#       Usage:          ./check_ram.sh
+#       Created By:     Hariprasath Ravichandran
+#       Created Date:   25/10/2014
+#       E-mail:         udthariprasath@gmail.com
+#       License:        GNU GPL
+#       More Plugins:   https://github.com/udthariprasath/
+#
+#*-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**
 SHELL=/bin/bash
 PATH=/usr/lib/qt-3.3/bin:/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
 warn=90
